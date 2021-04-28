b41ca7ca715303ff423b074fa89353ffe8b67137
Todor Valchev <tvalchev84@gmail.com>
2021-04-28 21:54:14 +0300

    Created web project section

--- components/web_projects.php
+++ components/web_projects.php
@@ -1,7 +1,52 @@
 <section class="web_projects">
 
-    <div>
-        Web Projects
+    <div class="u-center-text u-margin-bottom-small u-margin-top-small">
+        <h2 class="heading-2">Web Projects</h2>
     </div>
+
+    <div class="web_projects__container">
+
+        <figure class="web_projects__container__item">
+            <a title="ClioTravel LTD website"
+                alt="ClioTravel LTD website" 
+                href="https://clio-travel.com/" target="_blank" rel="noreferrer">
+                <picture>
+                    <source srcset="resources/img/todor_valchev_cliotravel.webp" type="image/webp">
+                    <source srcset="resources/img/todor_valchev_cliotravel.jpg" type="image/jpeg"> 
+                    <img title="Todor Valchev - ClioTravel WebSite" 
+                        src="resources/img/todor_valchev_cliotravel.jpg" 
+                        alt="Todor Valchev - ClioTravel WebSite" 
+                        class="web_projects__container__item__img" >
+                </picture>
+                <figcaption class="web_projects__container__item__caption"> 
+                    <h3>ClioTravel</h3>
+                    <br>
+                    <p>Build the website for local travel agency</p>
+                    <p class="u-margin-top-small"><strong>Tags:</strong> <br />Design, Development, PHP, JS, HTML, CSS</p>
+                </figcaption>
+            </a>
+        </figure>
+        <figure class="web_projects__container__item">
+            <a title="Predistic LTD IOT website"
+                alt="Predistic LTD IOT website" 
+                href="https://iot.predistic.com/" target="_blank" rel="noreferrer">
+                <picture>
+                    <source srcset="resources/img/todor_valchev_predistic.webp" type="image/webp">
+                    <source srcset="resources/img/todor_valchev_predistic.jpg" type="image/jpeg"> 
+                    <img title="Todor Valchev develop Predistic WebSite" 
+                        src="resources/img/todor_valchev_predistic.jpg" 
+                        alt="Todor Valchev develop Predistic WebSite " 
+                        class="web_projects__container__item__img" >
+                </picture>
+                <figcaption class="web_projects__container__item__caption">
+                    <h3>Iot Predistic</h3>
+                    <br>
+                    <p>Build the website for Predistic LTD</p>
+                    <p class="u-margin-top-small"><strong>Tags:</strong> <br />Development, ReactJS, JS, HTML, CSS</p>
+                </figcaption>
+            </a>
+        </figure>
+    </div>
+
 
 </section>
