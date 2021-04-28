06a3a92ecaff9509a0feeab8a7b912ceb54b7ccf
Todor Valchev <tvalchev84@gmail.com>
2021-04-28 22:04:30 +0300

    Fixed some design issues.

--- components/gallery.php
+++ components/gallery.php
@@ -1,100 +1,106 @@
-<section class="gallery">
-    <figure class="gallery__item gallery__item--1">
-        <img title="Todor Valchev image gallery 1"
-            alt="Todor Valchev image gallery 1"
-            data-src="resources/img/todor_valchev_gal-1.jpg"
-            data-webp="resources/img/todor_valchev_gal-1.webp"
-            class="gallery__img lazy-img">
-    </figure>
-    <figure class="gallery__item gallery__item--2">
-        <img title="Todor Valchev image gallery 2"
-            alt="Todor Valchev image gallery 2"
-            data-src="resources/img/todor_valchev_gal-2.jpg"
-            data-webp="resources/img/todor_valchev_gal-2.webp"
-            class="gallery__img lazy-img">
-    </figure>
-    <figure class="gallery__item gallery__item--3">
-        <img title="Todor Valchev image gallery 3"
-            alt="Todor Valchev image gallery 3"
-            data-src="resources/img/todor_valchev_gal-3.jpg"
-            data-webp="resources/img/todor_valchev_gal-3.webp"
-            class="gallery__img lazy-img">
-    </figure>
-    <figure class="gallery__item gallery__item--4">
-        <img title="Todor Valchev image gallery 4"
-            alt="Todor Valchev image gallery 4"
-            data-src="resources/img/todor_valchev_gal-4.jpg"
-            data-webp="resources/img/todor_valchev_gal-4.webp"
-            class="gallery__img lazy-img">
-    </figure>
-    <figure class="gallery__item gallery__item--5">
-        <img title="Todor Valchev image gallery 5"
-            alt="Todor Valchev image gallery 5"
-            data-src="resources/img/todor_valchev_gal-5.jpg"
-            data-webp="resources/img/todor_valchev_gal-5.webp"
-            class="gallery__img lazy-img">
-    </figure>
-    <figure class="gallery__item gallery__item--6">
-        <img title="Todor Valchev image gallery 6"
-            alt="Todor Valchev image gallery 6"
-            data-src="resources/img/todor_valchev_gal-6.jpg"
-            data-webp="resources/img/todor_valchev_gal-6.webp"
-            class="gallery__img lazy-img">
-    </figure>
-    <figure class="gallery__item gallery__item--7">
-        <img title="Todor Valchev image gallery 7"
-            alt="Todor Valchev image gallery 7"
-            data-src="resources/img/todor_valchev_gal-7.jpg"
-            data-webp="resources/img/todor_valchev_gal-7.webp"
-            class="gallery__img lazy-img">
-    </figure>
-    <figure class="gallery__item gallery__item--8">
-        <img title="Todor Valchev image gallery 8"
-            alt="Todor Valchev image gallery 8"
-            data-src="resources/img/todor_valchev_gal-8.jpg"
-            data-webp="resources/img/todor_valchev_gal-8.webp"
-            class="gallery__img lazy-img">
-    </figure>
-    <figure class="gallery__item gallery__item--9">
-        <img title="Todor Valchev image gallery 9"
-            alt="Todor Valchev image gallery 9"
-            data-src="resources/img/todor_valchev_gal-9.jpg"
-            data-webp="resources/img/todor_valchev_gal-9.webp"
-            class="gallery__img lazy-img">
-    </figure>
-    <figure class="gallery__item gallery__item--10">
-        <img title="Todor Valchev image gallery 10"
-            alt="Todor Valchev image gallery 10"
-            data-src="resources/img/todor_valchev_gal-10.jpg"
-            data-webp="resources/img/todor_valchev_gal-10.webp"
-            class="gallery__img lazy-img">
-    </figure>
-    <figure class="gallery__item gallery__item--11">
-        <img title="Todor Valchev image gallery 11"
-            alt="Todor Valchev image gallery 11"
-            data-src="resources/img/todor_valchev_gal-11.jpg"
-            data-webp="resources/img/todor_valchev_gal-11.webp"
-            class="gallery__img lazy-img">
-    </figure>
-    <figure class="gallery__item gallery__item--12">
-        <img title="Todor Valchev image gallery 12"
-            alt="Todor Valchev image gallery 12"
-            data-src="resources/img/todor_valchev_gal-12.jpg"
-            data-webp="resources/img/todor_valchev_gal-12.webp"
-            class="gallery__img lazy-img">
-    </figure>
-    <figure class="gallery__item gallery__item--13">
-        <img title="Todor Valchev image gallery 13"
-            alt="Todor Valchev image gallery 13"
-            data-src="resources/img/todor_valchev_gal-13.jpg"
-            data-webp="resources/img/todor_valchev_gal-13.webp"
-            class="gallery__img lazy-img">
-    </figure>
-    <figure class="gallery__item gallery__item--14">
-        <img title="Todor Valchev image gallery 14"
-            alt="Todor Valchev image gallery 14"
-            data-src="resources/img/todor_valchev_gal-14.jpg"
-            data-webp="resources/img/todor_valchev_gal-14.webp"
-            class="gallery__img lazy-img">
-    </figure>
+<section class="gallery_section">
+    <div class="u-center-text u-margin-bottom-small u-margin-top-small">
+        <h2 class="heading-2">Gallery</h2>
+    </div>
+    <div class="gallery">
+        <figure class="gallery__item gallery__item--1">
+            <img title="Todor Valchev image gallery 1"
+                alt="Todor Valchev image gallery 1"
+                data-src="resources/img/todor_valchev_gal-1.jpg"
+                data-webp="resources/img/todor_valchev_gal-1.webp"
+                class="gallery__img lazy-img">
+        </figure>
+        <figure class="gallery__item gallery__item--2">
+            <img title="Todor Valchev image gallery 2"
+                alt="Todor Valchev image gallery 2"
+                data-src="resources/img/todor_valchev_gal-2.jpg"
+                data-webp="resources/img/todor_valchev_gal-2.webp"
+                class="gallery__img lazy-img">
+        </figure>
+        <figure class="gallery__item gallery__item--3">
+            <img title="Todor Valchev image gallery 3"
+                alt="Todor Valchev image gallery 3"
+                data-src="resources/img/todor_valchev_gal-3.jpg"
+                data-webp="resources/img/todor_valchev_gal-3.webp"
+                class="gallery__img lazy-img">
+        </figure>
+        <figure class="gallery__item gallery__item--4">
+            <img title="Todor Valchev image gallery 4"
+                alt="Todor Valchev image gallery 4"
+                data-src="resources/img/todor_valchev_gal-4.jpg"
+                data-webp="resources/img/todor_valchev_gal-4.webp"
+                class="gallery__img lazy-img">
+        </figure>
+        <figure class="gallery__item gallery__item--5">
+            <img title="Todor Valchev image gallery 5"
+                alt="Todor Valchev image gallery 5"
+                data-src="resources/img/todor_valchev_gal-5.jpg"
+                data-webp="resources/img/todor_valchev_gal-5.webp"
+                class="gallery__img lazy-img">
+        </figure>
+        <figure class="gallery__item gallery__item--6">
+            <img title="Todor Valchev image gallery 6"
+                alt="Todor Valchev image gallery 6"
+                data-src="resources/img/todor_valchev_gal-6.jpg"
+                data-webp="resources/img/todor_valchev_gal-6.webp"
+                class="gallery__img lazy-img">
+        </figure>
+        <figure class="gallery__item gallery__item--7">
+            <img title="Todor Valchev image gallery 7"
+                alt="Todor Valchev image gallery 7"
+                data-src="resources/img/todor_valchev_gal-7.jpg"
+                data-webp="resources/img/todor_valchev_gal-7.webp"
+                class="gallery__img lazy-img">
+        </figure>
+        <figure class="gallery__item gallery__item--8">
+            <img title="Todor Valchev image gallery 8"
+                alt="Todor Valchev image gallery 8"
+                data-src="resources/img/todor_valchev_gal-8.jpg"
+                data-webp="resources/img/todor_valchev_gal-8.webp"
+                class="gallery__img lazy-img">
+        </figure>
+        <figure class="gallery__item gallery__item--9">
+            <img title="Todor Valchev image gallery 9"
+                alt="Todor Valchev image gallery 9"
+                data-src="resources/img/todor_valchev_gal-9.jpg"
+                data-webp="resources/img/todor_valchev_gal-9.webp"
+                class="gallery__img lazy-img">
+        </figure>
+        <figure class="gallery__item gallery__item--10">
+            <img title="Todor Valchev image gallery 10"
+                alt="Todor Valchev image gallery 10"
+                data-src="resources/img/todor_valchev_gal-10.jpg"
+                data-webp="resources/img/todor_valchev_gal-10.webp"
+                class="gallery__img lazy-img">
+        </figure>
+        <figure class="gallery__item gallery__item--11">
+            <img title="Todor Valchev image gallery 11"
+                alt="Todor Valchev image gallery 11"
+                data-src="resources/img/todor_valchev_gal-11.jpg"
+                data-webp="resources/img/todor_valchev_gal-11.webp"
+                class="gallery__img lazy-img">
+        </figure>
+        <figure class="gallery__item gallery__item--12">
+            <img title="Todor Valchev image gallery 12"
+                alt="Todor Valchev image gallery 12"
+                data-src="resources/img/todor_valchev_gal-12.jpg"
+                data-webp="resources/img/todor_valchev_gal-12.webp"
+                class="gallery__img lazy-img">
+        </figure>
+        <figure class="gallery__item gallery__item--13">
+            <img title="Todor Valchev image gallery 13"
+                alt="Todor Valchev image gallery 13"
+                data-src="resources/img/todor_valchev_gal-13.jpg"
+                data-webp="resources/img/todor_valchev_gal-13.webp"
+                class="gallery__img lazy-img">
+        </figure>
+        <figure class="gallery__item gallery__item--14">
+            <img title="Todor Valchev image gallery 14"
+                alt="Todor Valchev image gallery 14"
+                data-src="resources/img/todor_valchev_gal-14.jpg"
+                data-webp="resources/img/todor_valchev_gal-14.webp"
+                class="gallery__img lazy-img">
+        </figure>
+    </div>
+
 </section>
