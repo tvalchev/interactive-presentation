ab59c3222d118231e8f08bf41557b4dedb1faa29
Todor Valchev <tvalchev84@gmail.com>
2021-04-28 22:13:00 +0300

    Added prefix and minimize the css file.
    - update meta descriptions

--- components/head.php
+++ components/head.php
@@ -8,7 +8,13 @@
     <link href="https://fonts.googleapis.com/css?family=Josefin+Sans:300,400,400i|Nunito:300,300i" rel="preload" as="style">
     <link rel="stylesheet" href="resources/css/style.css">
 
-    <title>Todor Valchev CV</title>
+    <title>Todor Valchev Interactive CV | Software Developer</title>
+    <meta name="description" content="As a software developer I enjoy being challenged and engaging with projects that require me to work outside my comfort and knowledge set!" />
+    <meta property="og:title" content="Todor Valchev Interactive CV | Software Developer" />
+    <meta property="og:description" content="As a software developer I enjoy being challenged and engaging with projects that require me to work outside my comfort and knowledge set!" />
+    <meta name="google" content="notranslate">
+    <meta property="og:locale" content="bg_BG" />
+    <link rel="canonical" href=""/>
 
     <!-- Jquery  -->
 	  <script defer src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
