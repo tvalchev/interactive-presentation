5db380bf710b073e4d3c3d2ec0eaa96b398d160a
Todor Valchev <tvalchev84@gmail.com>
2021-04-28 14:44:26 +0300

    Prepare timeline for experiences section

--- components/experiences.php
+++ components/experiences.php
@@ -1,11 +1,92 @@
 <section class="experiences">
 
     <div>
-        Educations
+        <div class="u-center-text u-margin-bottom-small u-margin-top-small">
+            <h2 class="heading-2" style="color:white;">Education</h2>
+        </div>
+        <div class="timeline">
+            <div class="timeline__container timeline__left">
+                <div class="timeline__content">
+                <h3>2017</h3>
+                <p>Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per, habeo iusto primis ea eam.</p>
+                </div>
+            </div>
+            <div class="timeline__container timeline__right">
+                <div class="timeline__content">
+                <h2>2016</h2>
+                <p>Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per, habeo iusto primis ea eam.</p>
+                </div>
+            </div>
+            <div class="timeline__container timeline__left">
+                <div class="timeline__content">
+                <h2>2015</h2>
+                <p>Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per, habeo iusto primis ea eam.</p>
+                </div>
+            </div>
+            <div class="timeline__container timeline__right">
+                <div class="timeline__content">
+                <h2>2012</h2>
+                <p>Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per, habeo iusto primis ea eam.</p>
+                </div>
+            </div>
+            <div class="timeline__container timeline__left">
+                <div class="timeline__content">
+                <h2>2011</h2>
+                <p>Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per, habeo iusto primis ea eam.</p>
+                </div>
+            </div>
+            <div class="timeline__container timeline__right">
+                <div class="timeline__content">
+                <h2>2007</h2>
+                <p>Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per, habeo iusto primis ea eam.</p>
+                </div>
+            </div>
+        </div>
     </div>
 
+
     <div>
-        Careers
+        <div class="u-center-text u-margin-bottom-small u-margin-top-small">
+            <h2 class="heading-2" style="color:white;">Work Experience</h2>
+        </div>
+        <div class="timeline">
+            <div class="timeline__container timeline__left">
+                <div class="timeline__content">
+                <h3>2017</h3>
+                <p>Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per, habeo iusto primis ea eam.</p>
+                </div>
+            </div>
+            <div class="timeline__container timeline__right">
+                <div class="timeline__content">
+                <h2>2016</h2>
+                <p>Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per, habeo iusto primis ea eam.</p>
+                </div>
+            </div>
+            <div class="timeline__container timeline__left">
+                <div class="timeline__content">
+                <h2>2015</h2>
+                <p>Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per, habeo iusto primis ea eam.</p>
+                </div>
+            </div>
+            <div class="timeline__container timeline__right">
+                <div class="timeline__content">
+                <h2>2012</h2>
+                <p>Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per, habeo iusto primis ea eam.</p>
+                </div>
+            </div>
+            <div class="timeline__container timeline__left">
+                <div class="timeline__content">
+                <h2>2011</h2>
+                <p>Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per, habeo iusto primis ea eam.</p>
+                </div>
+            </div>
+            <div class="timeline__container timeline__right">
+                <div class="timeline__content">
+                <h2>2007</h2>
+                <p>Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per, habeo iusto primis ea eam.</p>
+                </div>
+            </div>
+        </div>
     </div>
 
 </section>
